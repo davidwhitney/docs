a3f1d35eb9bca8dd5f280f4844e722172d1fc24a
David Whitney <david@davidwhitney.co.uk>
2025-08-31 22:22:24 +0100

    Hacky routing

--- middleware/functions/docs.ts
+++ middleware/functions/docs.ts
@@ -1,11 +1,28 @@
 import { RequestHandler } from "lume/core/server.ts";
 
+const reference: Record<string, Record<string, any>> = {
+  "deno": JSON.parse(Deno.readTextFileSync("./reference_gen/gen/deno.json")),
+  "node": JSON.parse(Deno.readTextFileSync("./reference_gen/gen/node.json")),
+  "web": JSON.parse(Deno.readTextFileSync("./reference_gen/gen/web.json")),
+};
+
 const handler: RequestHandler = (req: Request): Promise<Response> => {
   const url = new URL(req.url);
-  const pathname = url.pathname;
-  console.log("API Docs requested for:", pathname);
+  const parts = url.pathname.split("/api2/");
+  const requestedDoc = parts[1];
 
-  return Promise.resolve(new Response("OK ok ok" + pathname, { status: 200 }));
+  const referenceFile = requestedDoc.split("/")[0];
+  const everythingElse = requestedDoc.substring(referenceFile.length + 1);
+
+  const lookupPath = "./" + everythingElse + ".json";
+  const file = reference[referenceFile];
+  const value = file[lookupPath];
+
+  return Promise.resolve(
+    new Response(JSON.stringify(value), {
+      status: 200,
+    }),
+  );
 };
 
 export default handler;
